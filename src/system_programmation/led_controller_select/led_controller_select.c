d7df19af32a7bf55abaa0a1cc7af399229a639b4
Simon Corboz¨ <simon.corboz@edu.hefr.ch>
2022-04-29 14:16:25 +0000

    added periodic blink

--- src/system_programmation/led_controller_select/led_controller_select.c
+++ src/system_programmation/led_controller_select/led_controller_select.c
@@ -30,7 +30,27 @@ int freq = 2;//Base
 //IN : button descriptor : nullable
 typedef void (*callback_t)(char*);
 //we are going to monitor the 3 buttons for input events
+static int open_led()
+{
+	// unexport pin out of sysfs (reinitialization)
+	int f = open (GPIO_UNEXPORT, O_WRONLY);
+	write (f, LED, strlen(LED));
+	close (f);
 
+	// export pin to sysfs
+	f = open (GPIO_EXPORT, O_WRONLY);
+	write (f, LED, strlen(LED));
+	close (f);
+
+	// config pin
+	f = open (GPIO_LED "/direction", O_WRONLY);
+	write (f, "out", 3);
+	close (f);
+
+	// open gpio value attribute
+ 	f = open (GPIO_LED "/value", O_RDWR);
+	return f;
+}
 static void reset_gpios(){
     // unexport pin out of sysfs (reinitialization)
 	int f = open (GPIO_UNEXPORT, O_WRONLY);
@@ -69,35 +89,43 @@ static void init_buttons()
  	write (f, "rising", 7);
     close(f);
 }
-static void init_led(){
-    // export pin to sysfs
-	int f = open (GPIO_EXPORT, O_WRONLY);
-	write (f, LED, strlen(LED));
-	close (f);
-
-	// config pin
-	f = open (GPIO_LED "/direction", O_WRONLY);
-	write (f, "out", 3);
-	close (f);
-}
 
 int main(int argc, char* argv[]){
     	reset_gpios();
         init_buttons();
-        init_led();
         int k1_fd = open(GPIO_K1 "/value", O_RDWR);
         int k2_fd = open(GPIO_K2 "/value", O_RDWR);
         int k3_fd = open(GPIO_K3 "/value", O_RDWR);
-        int led_fd = open(GPIO_LED, "/value", O_RDWR);
-        int millisecs = one_sec_in_millisec/freq;//base freq = 2hz
-        struct itimerspec timspec;
-        bzero(&timspec, sizeof(timspec));
-        timspec.it_interval.tv_sec = 0;
-        timspec.it_interval.tv_nsec = millisecs * 1000000;
-        timspec.it_value.tv_sec = 0;
-        timspec.it_value.tv_nsec = 1;
+        int led_fd = open_led();
+    
         //we need to first multiplex the 3 button
+        int tfd = timerfd_create(CLOCK_REALTIME,0);
+        if(tfd > 0 ){
+            char dummybuf[8];
+            struct itimerspec spec =
+            {
+                { 2, 0 }, // Set to {0, 0} if you need a one-shot timer
+                { 4, 0 }
+            };
+            timerfd_settime(tfd, 0, &spec, NULL);
+            fd_set timer_fd_set;
+            int retval;         
+            FD_ZERO(&timer_fd_set);
+            FD_SET(0, &timer_fd_set);
+            FD_SET(tfd, &timer_fd_set);
+            int limit = 100;
+            int i = 0;
+            while(i < limit){
+                retval = select(tfd+1, &timer_fd_set, NULL, NULL, NULL);
+                printf("Expired at %d! (%d) (%d)\n", (int)time(NULL), retval, read(tfd, dummybuf, 8) );
+                if (i%2 == 0)
+				    pwrite (led_fd, "1", sizeof("1"), 0);
+			    else
+				    pwrite (led_fd, "0", sizeof("0"), 0);
+                i++;
+            }
 
+        }
         //then wait on
 
 }
