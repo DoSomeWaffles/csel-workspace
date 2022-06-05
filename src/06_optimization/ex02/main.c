51df9d153b46c1dd607b3f759f3d446a073f95cd
Simon Corboz¨ <simon.corboz@edu.hefr.ch>
2022-06-05 17:10:04 +0000

    finished opti

--- src/06_optimization/ex02/main.c
+++ src/06_optimization/ex02/main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 
 #define SIZE 65536
-
+static int compare(const void *a, const void* b){
+	return *(short*)a - *(short*)b;
+}
 int main()
 {
+
  	// generate data
 	short data[SIZE];
 	for (int i = 0; i < SIZE; i++) {
 		data[i] = rand() % 512;
 	}
-
+	qsort(data,SIZE,sizeof(data[0]),compare);
 
 	long long sum = 0;
 	for (int j = 0; j < 10000; j++) {
