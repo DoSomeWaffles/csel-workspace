9103091818fbfae404efc785f8659bd7d5984d7d
Simon Corboz¨ <simon.corboz@edu.hefr.ch>
2022-06-05 14:12:47 +0000

    fdasf

--- src/pilotes/ex7_fatal_input_output/driver/skeleton.c
+++ src/pilotes/ex7_fatal_input_output/driver/skeleton.c
@@ -18,31 +18,13 @@
 #define K2 2
 #define K3 3
 
-<<<<<<< HEAD
-static char *k1 = "gpio_a.0-k1";
-static char *k2 = "gpio_a.2-k2";
-static char *k3 = "gpio_a.3-k3";
-=======
 static char* k1 = "gpio_a.0-k1";
 static char* k2 = "gpio_a.2-k2";
 static char* k3 = "gpio_a.3-k3";
->>>>>>> ex3+4
 
 static atomic_t nb_of_interrupts;
 DECLARE_WAIT_QUEUE_HEAD(queue);
 
-<<<<<<< HEAD
-// interrupt handling
-irqreturn_t gpio_isr(int irq, void *handle)
-{
-    atomic_inc(&nb_of_interrupts);
-    wake_up_interruptible(&queue);
-    pr_info("interrupt %s raised...\n", (char *)handle);
-    return IRQ_HANDLED;
-}
-
-static ssize_t skeleton_read(struct file *f, char __user *buf, size_t sz, loff_t *off)
-=======
 irqreturn_t gpio_isr(int irq, void* handle)
 {
     atomic_inc(&nb_of_interrupts);
@@ -57,20 +39,10 @@ static ssize_t skeleton_read(struct file* f,
                              char __user* buf,
                              size_t sz,
                              loff_t* off)
->>>>>>> ex3+4
 {
     return 0;
 }
 
-<<<<<<< HEAD
-static unsigned int skeleton_poll(struct file *f, poll_table *wait)
-{
-    unsigned mask = 0;
-    poll_wait(f, &queue, wait);
-    if (atomic_read(&nb_of_interrupts) != 0)
-    {
-        mask |= POLLIN | POLLRDNORM; 
-=======
 static unsigned int skeleton_poll(struct file* f, poll_table* wait)
 {
     unsigned mask = 0;
@@ -78,7 +50,6 @@ static unsigned int skeleton_poll(struct file* f, poll_table* wait)
     if (atomic_read(&nb_of_interrupts) != 0) {
         mask |= POLLIN | POLLRDNORM; /* read operation */
         /* mask |= POLLOUT | POLLWRNORM;   write operation */
->>>>>>> ex3+4
         atomic_dec(&nb_of_interrupts);
         pr_info("polling thread waked-up...\n");
     }
@@ -86,46 +57,21 @@ static unsigned int skeleton_poll(struct file* f, poll_table* wait)
 }
 
 static struct file_operations skeleton_fops = {
-<<<<<<< HEAD
-    .owner = THIS_MODULE, // owner of the module
-    .read = skeleton_read, // read method
-    .poll = skeleton_poll, // poll method 
-=======
     .owner = THIS_MODULE,
     .read  = skeleton_read,
     .poll  = skeleton_poll,
->>>>>>> ex3+4
 };
 
 struct miscdevice misc_device = {
     .minor = MISC_DYNAMIC_MINOR,
-<<<<<<< HEAD
-    .fops = &skeleton_fops,
-    .name = "mymodule",
-    .mode = 0777,
-=======
     .fops  = &skeleton_fops,
     .name  = "mymodule",
     .mode  = 0777,
->>>>>>> ex3+4
 };
 
 static int __init skeleton_init(void)
 {
     int status = 0;
-<<<<<<< HEAD
-    atomic_set(&nb_of_interrupts, 0);
-    status = misc_register(&misc_device);
-    // install k1
-    if (status == 0)
-        status = devm_request_irq(misc_device.this_device, gpio_to_irq(K1), gpio_isr, IRQF_TRIGGER_FALLING | IRQF_SHARED, k1, k1);
-    // install k2
-    if (status == 0)
-        status = devm_request_irq(misc_device.this_device, gpio_to_irq(K2), gpio_isr, IRQF_TRIGGER_RISING | IRQF_SHARED, k2, k2);
-    // install k3
-    if (status == 0)
-        status = devm_request_irq(misc_device.this_device, gpio_to_irq(K3), gpio_isr, IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING | IRQF_SHARED, k3, k3);
-=======
 
     atomic_set(&nb_of_interrupts, 0);
 
@@ -159,7 +105,6 @@ static int __init skeleton_init(void)
             k3,
             k3);
 
->>>>>>> ex3+4
     pr_info("Linux module skeleton loaded(status=%d) ex7\n", status);
     return status;
 }
@@ -173,10 +118,6 @@ static void __exit skeleton_exit(void)
 module_init(skeleton_init);
 module_exit(skeleton_exit);
 
-<<<<<<< HEAD
-MODULE_AUTHOR("Daniel Gachet <daniel.gachet@hefr.ch> modified by Denis Rosset <denis.rosset@hes-so.ch> and Simon Corboz <simon.corboz@hes-so.ch>");
-=======
 MODULE_AUTHOR("Daniel Gachet <daniel.gachet@hefr.ch>");
->>>>>>> ex3+4
 MODULE_DESCRIPTION("Module skeleton");
 MODULE_LICENSE("GPL");
